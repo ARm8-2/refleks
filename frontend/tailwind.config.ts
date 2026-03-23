20c6ea18ce72a6266becc4118c3bd9b3fa0240c6
ARm8-2 <77163307+ARm8-2@users.noreply.github.com>
2026-03-23 18:24:22 +0000

    Add Tailwind CSS configuration with custom theme and plugins

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 import tailwindcssAnimate from 'tailwindcss-animate'
+import { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
   content: [
@@ -102,4 +102,4 @@ export default {
     },
   },
   plugins: [tailwindcssAnimate],
-}
+} satisfies Config
