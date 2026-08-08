768318d41766f285e6a86e14a29ce7ca362eeb26
ARm8 <77163307+ARm8-2@users.noreply.github.com>
2026-08-08 19:51:15 +0200

    Bump version to 0.9.5

--- internal/constants/version.go
+++ internal/constants/version.go
@@ -2,4 +2,4 @@ package constants
 
 // AppVersion is the human-readable semantic version of the application.
 // Bump this on every release. Follow SemVer: MAJOR.MINOR.PATCH
-const AppVersion = "0.9.4"
+const AppVersion = "0.9.5"
